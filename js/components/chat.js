38cb7152b7c6f971734c0149cc0429c9dc00dd5a
Chiaki-Suzuki <c.suzuki.eba@gmail.com>
2022-09-07 17:12:44 +0900

    質問のメッセージ表示パターンを条件分岐に変更し、一つの関数にまとめる

--- js/components/chat.js
+++ js/components/chat.js
@@ -1,66 +1,66 @@
 Vue.component('chat', {
   template: `
   <div>
-    <q2 v-if="q2box"
+    <question v-if="q2box"
         v-bind:question="questions"
         v-bind:num="num"
-        v-on:simpleQue="simpleQue">
-    </q2>
-    <q2 v-if="q3box"
+        v-on:newMsg="newMsg">
+    </question>
+    <question v-if="q3box"
         v-bind:question="questions"
         v-bind:num="num"
-        v-on:doubleQue="doubleQue">
-    </q2>
-    <q2 v-if="q4box"
+        v-on:newMsg="newMsg">
+    </question>
+    <question v-if="q4box"
         v-bind:question="questions"
         v-bind:num="num"
-        v-on:simpleQue="simpleQue">
-    </q2>
-    <q2 v-if="q5box"
+        v-on:newMsg="newMsg">
+    </question>
+    <question v-if="q5box"
         v-bind:question="questions"
         v-bind:num="num"
-        v-on:simpleQue="simpleQue">
-    </q2>
-    <q2 v-if="q6box"
+        v-on:newMsg="newMsg">
+    </question>
+    <question v-if="q6box"
         v-bind:question="questions"
         v-bind:num="num"
-        v-on:multiQue="multiQue">
-    </q2>
-    <q2 v-if="q7box"
+        v-on:newMsg="newMsg">
+    </question>
+    <question v-if="q7box"
         v-bind:question="questions"
         v-bind:num="num"
-        v-on:multiQue="multiQue">
-    </q2>
-    <q2 v-if="q8box"
+        v-on:newMsg="newMsg">
+    </question>
+    <question v-if="q8box"
         v-bind:question="questions"
         v-bind:num="num"
-        v-on:multiQue="multiQue">
-    </q2>
-    <q2 v-if="q9box"
+        v-on:newMsg="newMsg">
+    </question>
+    <question v-if="q9box"
         v-bind:question="questions"
         v-bind:num="num"
-        v-on:multiQue="multiQue">
-    </q2>
-    <q2 v-if="q10box"
+        v-on:newMsg="newMsg">
+    </question>
+    <question v-if="q10box"
         v-bind:question="questions"
         v-bind:num="num"
-        v-on:simpleQue="simpleQue">
-    </q2>
-    <q2 v-if="q11box"
+        v-on:newMsg="newMsg">
+    </question>
+    <question v-if="q11box"
         v-bind:question="questions"
         v-bind:num="num"
-        v-on:simpleQue="simpleQue">
-    </q2>
+        v-on:newMsg="newMsg">
+    </question>
     <pref v-if="prefShow && typeof questions !== 'undefined'"
         v-bind:region="region"
         v-bind:prefId="prefId"
         v-on:prefSelect="prefSelect"
-        v-on:multiQue="multiQue">
+        v-on:newMsg="newMsg">
     </pref>
   </div>
   `,
   components: {
-    'q2': q2,
+    'question': question,
     'pref': pref
   },
   props: ['q2box', 'noDom', 'icon', 'loadingAnimation'],
@@ -92,9 +92,9 @@ Vue.component('chat', {
   },
   methods: {
     /*-------------------------
-      質問のみ
+      メッセージを表示
     -------------------------*/
-    simpleQue: function (queNum, ansNum, gobi, question) {
+    newMsg: function (id, queNum, ansNum, gobi, msg1, msg2, msg3) {
       let chatbox = document.querySelectorAll('.chatbox');
       let ans = document.querySelectorAll('.chatbox .quiestion button');
       // ボタンを消す
@@ -102,94 +102,71 @@ Vue.component('chat', {
 
       // メッセージ１
       this.$emit('child-msg', chatbox[queNum], this.noDom, 'user', 0, `${ans[ansNum].innerHTML}${gobi}`, 500)
-      // メッセージ２＋次の質問を表示＋自動スクロール
-      this.$emit('child-msg', chatbox[queNum], this.icon, 'guide', 0, question, 2500, () => this.nextQue(queNum), queNum)
-    },
-    /*-------------------------
-      メッセージ＋質問
-    -------------------------*/
-    multiQue: function (queNum, ansNum, gobi, msg, question) {
-      let chatbox = document.querySelectorAll('.chatbox');
-      let ans = document.querySelectorAll('.chatbox .quiestion button');
-      // ボタンを消す
-      this.btnsNoDisp(chatbox[queNum]);
 
-      // メッセージ１
-      this.$emit('child-msg', chatbox[queNum], this.noDom, 'user', 0, `${ans[ansNum].innerHTML}${gobi}`, 500)
-      // メッセージ２
-      this.$emit('child-msg', chatbox[queNum], this.icon, 'guide', 0, msg, 2500)
-      // メッセージ３＋次の質問を表示＋自動スクロール
-      // 最後の場合は次の質問は実行しない
-      if (queNum === 11) {
-        this.$emit('child-msg', chatbox[queNum], this.noDom, 'noicon_guide', 0, question, 5000)
-      } else {
-        this.$emit('child-msg', chatbox[queNum], this.noDom, 'noicon_guide', 0, question, 5000, () => this.nextQue(queNum), queNum)
+      // メッセージ２まで
+      if (msg2 === '' && msg2 === '') {
+        // メッセージ２＋次の質問を表示＋自動スクロール
+        this.$emit('child-msg', chatbox[queNum], this.icon, 'guide', 0, msg1, 2500, () => this.nextQue(queNum), queNum)
+        return;
       }
-    },
-    /*-------------------------
-      質問＋メッセージ＋質問
-    -------------------------*/
-    doubleQue: function (queNum, ansNum, gobi, question1, msg1, question2) {
-      let chatbox = document.querySelectorAll('.chatbox');
-      let ans = document.querySelectorAll('.chatbox .quiestion button');
-      // ボタンを消す
-      this.btnsNoDisp(chatbox[queNum]);
 
-      // メッセージ１
-      this.$emit('child-msg', chatbox[queNum], this.noDom, 'user', 0, `${ans[ansNum].innerHTML}${gobi}`, 500)
       // メッセージ２
-      this.$emit('child-msg', chatbox[queNum], this.icon, 'guide', 0, question1, 2500)
+      this.$emit('child-msg', chatbox[queNum], this.icon, 'guide', 0, msg1, 2500)
+
+      // メッセージ３まで
+      if (msg3 === '') {
+        if (queNum === 11) {
+          this.$emit('child-msg', chatbox[queNum], this.noDom, 'noicon_guide', 0, msg2, 5000)
+        } else {
+          this.$emit('child-msg', chatbox[queNum], this.noDom, 'noicon_guide', 0, msg2, 5000, () => this.nextQue(queNum), queNum)
+        }
+        return;
+      }
+
       // メッセージ３
-      this.$emit('child-msg', chatbox[queNum], this.icon, 'guide', 1, msg1, 5000)
+      this.$emit('child-msg', chatbox[queNum], this.icon, 'guide', 1, msg2, 5000)
       // メッセージ４＋次の質問を表示＋自動スクロール
-      this.$emit('child-msg', chatbox[queNum], this.noDom, 'noicon_guide', 0, question2, 7500, () => this.nextQue(queNum), queNum)
+      this.$emit('child-msg', chatbox[queNum], this.noDom, 'noicon_guide', 0, msg3, 7500, () => this.nextQue(queNum), queNum)
     },
     /*-------------------------
       ボタン非表示
     -------------------------*/
     btnsNoDisp: function (parent) {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          let btns = document.querySelector('.chatbox .quiestion')
-          parent.removeChild(btns)
-          resolve();
-        }, 500)
-      })
+      setTimeout(() => {
+        let btns = document.querySelector('.chatbox .quiestion')
+        parent.removeChild(btns)
+      }, 500)
     },
     /*-------------------------
       次の質問を表示
     -------------------------*/
     nextQue: function (num, sec) {
-      return new Promise(resolve => {
-        let self = this;
-        setTimeout(() => {
-          if (num === '1') {
-            self.q3box = true;
-          } else if (num === '2') {
-            self.q4box = true;
-          } else if (num === '3') {
-            self.q5box = true;
-          } else if (num === '4') {
-            self.q6box = true;
-          } else if (num === '5') {
-            self.q7box = true;
-          } else if (num === '6') {
-            self.q8box = true;
-          } else if (num === '7') {
-            self.q9box = true;
-          } else if (num === '8') {
-            self.q10box = true;
-          } else if (num === '9') {
-            self.q11box = true;
-          } else if (num === '10') {
-            self.prefShow = true;
-            return;
-          }
-          // 質問の表示を繰り返したい
-          self.num = num;
-        }, 2000)
-        resolve();
-      })
+      setTimeout(() => {
+        if (num === '1') {
+          this.q3box = true;
+        } else if (num === '2') {
+          this.q4box = true;
+        } else if (num === '3') {
+          this.q5box = true;
+        } else if (num === '4') {
+          this.q6box = true;
+        } else if (num === '5') {
+          this.q7box = true;
+        } else if (num === '6') {
+          this.q8box = true;
+        } else if (num === '7') {
+          this.q9box = true;
+        } else if (num === '8') {
+          this.q10box = true;
+        } else if (num === '9') {
+          this.q11box = true;
+        } else if (num === '10') {
+          this.prefShow = true;
+          return;
+        }
+        // 質問の表示を繰り返したい
+        this.num = num;
+      }, 2000)
     },
     /*-------------------------
       都道府県
---
+++ js/components/question.js
@@ -0,0 +1,12 @@
+let question = {
+  template: `
+  <div class="chatbox">
+      <div class="quiestion col3">
+          <button v-on:click="$emit('newMsg', question[num].id, question[num].len, 0, question[num].ending, question[num].msg1, question[num].msg2, question[num].msg3)">{{ question[num].choice1 }}</button>
+          <button v-on:click="$emit('newMsg', question[num].id, question[num].len, 1, question[num].ending, question[num].msg1, question[num].msg2, question[num].msg3)">{{ question[num].choice2 }}</button>
+          <button v-on:click="$emit('newMsg', question[num].id, question[num].len, 2, question[num].ending, question[num].msg1, question[num].msg2, question[num].msg3)">{{ question[num].choice3 }}</button>
+      </div>
+  </div>
+  `,
+  props: ['question', 'num']
+}
